a7fa303c8a67610d470f05fc0361467c1dd1d191
Jacob Ledbetter <ledbetterj1@wit.edu>
2023-06-08 11:44:40 -0400

    Added setup.py(Probably not set up right, don't use) and Requires.txt(Package installation)

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='Senior-Project',
+    version='3.10',
+    packages=['PyLaTeX', 'ordered-set'],
+    install_requires=['PyLaTeX'],
+    python_requires='>=3.10',
+    url='https://github.com/ledbetterj1atwit/Senior-Project',
+    license='MIT',
+    author='ledbetterj1atwit',
+    author_email='ledbetterj1@wit.edu',
+    description=''
+)
