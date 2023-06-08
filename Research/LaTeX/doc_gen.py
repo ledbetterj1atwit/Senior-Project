8a6a150df396e3c3ce8a3083a11bbd3239d7f056
Jacob Ledbetter <ledbetterj1@wit.edu>
2023-06-08 13:29:50 -0400

    Change gitignore, Got some working pdf generation.
    Nightmare, Nightmare, Nightmare, Nightmare, Nightmare, Nightmare, Nightmare, Nightmare, Nightmare, Nightmare, Nightmare, Nightmare, Nightmare.

---
+++ Research/LaTeX/doc_gen.py
@@ -0,0 +1,18 @@
+import pylatex
+from sys import argv
+
+if __name__ == "__main__":
+    path = "doc_gen.pdf"
+    try:
+        path = argv[1]
+    except IndexError:
+        pass
+    document = pylatex.Document()
+    with document.create(pylatex.Section("Section One")):
+        document.append(pylatex.MediumText("Hello, world."))
+        document.append("Im going to kill someone\n")
+        document.append("This took a fucking HOUR!!!")
+    with document.create(pylatex.Section("Section Two")):
+        document.append("Im getting lunch...")
+
+    document.generate_pdf(path, clean_tex=False)
