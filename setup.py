e34fba1c43734e92b44d7fa09499f3c32f47b318
Jacob Ledbetter <ledbetterj1@wit.edu>
2023-06-14 13:22:59 -0400

    update setup.py to match requires.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='Senior-Project',
     version='3.10',
-    packages=['PyLaTeX', 'ordered-set'],
+    packages=['PyLaTeX', 'ordered-set', 'PyQt6', 'PyQt6-tools'],
     install_requires=['PyLaTeX'],
     python_requires='>=3.10',
     url='https://github.com/ledbetterj1atwit/Senior-Project',
